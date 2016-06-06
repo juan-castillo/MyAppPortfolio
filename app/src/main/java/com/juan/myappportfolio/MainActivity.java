7654b48b6132b5f0dfa7e958ec00dd5445fda0ee
Juan Castillo <jcastillo345@gmail.com>
2016-06-06 14:04:49 -0400

    Initial Commit

---
+++ app/src/main/java/com/juan/myappportfolio/MainActivity.java
@@ -0,0 +1,24 @@
+package com.juan.myappportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Displays a toast message in response to a button click */
+    public void displayToast(View view) {
+        Button b = (Button) view;
+        CharSequence text = "This button will launch the " + b.getText().toString() + " app!";
+
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    }
+}
